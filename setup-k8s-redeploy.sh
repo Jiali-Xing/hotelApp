87052e975d464c0157639771fa799cf9d2221df4
邢嘉力 Jiali Xing <jiali.xing@protonmail.com>
2024-06-13 16:03:54 -0400

    [Debug] charon

--- setup-k8s-redeploy.sh
+++ setup-k8s-redeploy.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# Get all deployments and services excluding those with 'redis' in the name
-deployments=$(kubectl get deployments -o custom-columns=NAME:.metadata.name --no-headers | grep -v 'redis')
-services=$(kubectl get services -o custom-columns=NAME:.metadata.name --no-headers | grep -v 'redis')
 
 # inheren the environment variables DEBUG_INFO
 export DEBUG_INFO
 python ./scripts/gen-yaml.py
+
+# Get all deployments and services excluding those with 'redis' in the name
+deployments=$(kubectl get deployments -o custom-columns=NAME:.metadata.name --no-headers | grep -v 'redis')
+services=$(kubectl get services -o custom-columns=NAME:.metadata.name --no-headers | grep -v 'redis')
+
+sleep 5
 
 # Delete the filtered deployments and services
 for deployment in $deployments; do
@@ -16,6 +19,8 @@ done
 for service in $services; do
   kubectl delete service $service
 done
+
+sleep 5
 
 # Apply Kubernetes YAML files for services and Redis
 kubectl apply -f k8s/frontend-deployment.yaml
@@ -49,14 +54,14 @@ kubectl apply -f k8s/profile-redis-service.yaml
 echo "Kubernetes resources have been applied successfully."
 
 # wait for the pods to be ready
-# kubectl wait --for=condition=ready pod --all --timeout=60s
+kubectl wait --for=condition=ready pod --all --timeout=60s
 
-for port in {50051..50059}; do
-  pid=$(lsof -t -i :$port)
-  if [ -n "$pid" ]; then
-    kill -9 $pid
-  fi
-done
+# for port in {50051..50059}; do
+#   pid=$(lsof -t -i :$port)
+#   if [ -n "$pid" ]; then
+#     kill -9 $pid
+#   fi
+# done
 
 # Exit the script
 exit 0
