ca3838221f6154f3d40565921372761c5183857b
邢嘉力 Jiali Xing <jiali.xing@protonmail.com>
2024-08-20 20:09:50 -0400

    [bug fix] query address or hotel instead

--- setup-k8s-initial.sh
+++ setup-k8s-initial.sh
@@ -96,13 +96,13 @@ if [[ $METHOD == *"hotel"* ]]; then
   PORT_FORWARD_PID=$!
 
   # Wait for port-forwarding to be ready (adjust sleep time as needed)
-  sleep 25
+  # sleep 25
 
-  # query the frontend and user addresses from user input stdin
-  frontend_address=read -p "Enter the frontend address: "
-  user_address=read -p "Enter the user address: "
+  # Query the frontend and user addresses from user input
+  read -p "Enter the frontend address: " frontend_address
+  read -p "Enter the user address: " user_address
 
-  ./populate/populate --frontend=$frontend_address --user=$user_address --num_of_hotels=1000 --num_of_users=1000 --num_of_hotels_file=/users/jiali/hotelApp/experiments/hotel/data/hotels.json > popu.output 2>&1
+  ./populate/populate --frontend="$frontend_address" --user="$user_address" --num_of_hotels=1000 --num_of_users=1000 --num_of_hotels_file=/users/jiali/hotelApp/experiments/hotel/data/hotels.json > popu.output 2>&1
   # -hotels_file=/users/jiali/hotelApp/experiments/hotel/data/hotels.json -num_of_users=1000 > popu.output 2>&1
 
 elif [ "$METHOD" = "compose" -o "$METHOD" = "home-timeline" -o "$METHOD" = "user-timeline" -o "$METHOD" = "all-methods-social" ]; then
